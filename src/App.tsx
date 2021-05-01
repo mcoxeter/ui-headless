63cb36401b7383d7c4e36f0080198177e85ce605
Michael Coxeter <mcoxeter@gamil.com>
2021-05-01 10:28:06 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ButtonPrimaryHead } from './button/Button-PrimaryHead';
+import { ButtonSecondaryHead } from './button/Button-SecondaryHead';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return (<div>
+    <div><ButtonPrimaryHead caption={"Primary"} /></div>
+
+    <div><ButtonSecondaryHead caption={"Secondary"} /></div>    
+  </div>);
 }
 
 export default App;
---
+++ src/button/Button-SecondaryHead.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { ButtonHeadless } from './Button-Headless';
+import styles from './Button-SecondaryHead.module.scss';
+
+export interface ButtonSecondaryHeadProps {
+    caption: string;
+}
+
+export function ButtonSecondaryHead (props: ButtonSecondaryHeadProps) {
+    const [working, setWorking] = useState(false);
+
+    const simulateWork = () => {
+        setWorking(true);
+        setTimeout( () => setWorking(false), 500);
+    }
+  
+    return ( 
+            <ButtonHeadless working={working} onPress={simulateWork}>
+                    {
+                    (stateClassNames, spreadAttributes) => {
+                        const className = stateClassNames.map( x => styles[x]).join(' ');
+                        return (
+                            <button className={`${className} ${styles['component']}`} {...spreadAttributes}>
+                                <div className={styles['focus-ring']}>
+                                    <div className={styles['text']}>{props.caption}</div>                                    
+                                </div>
+                            </button>
+                        )
+                    }
+                }
+                </ButtonHeadless>
+            );
+  }
